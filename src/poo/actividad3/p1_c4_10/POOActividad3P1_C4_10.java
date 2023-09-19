508a4970b3a10de0df93f99c1c0e0c6e74699d5d
carol <carol@Carolina>
2023-09-18 22:57:23 -0500

    Ejercicio 14

---
+++ src/poo/actividad3/p1_c4_10/POOActividad3P1_C4_10.java
@@ -0,0 +1,10 @@
+package poo.actividad3.p1_c4_10;
+
+public class POOActividad3P1_C4_10 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
